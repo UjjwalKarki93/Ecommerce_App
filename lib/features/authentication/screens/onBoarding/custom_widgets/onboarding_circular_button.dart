d894628f22ecee425f398e9eb0b0f60ff18cc1ab
UjjwalKarki93 <karkiujjwal32@gmail.com>
2024-02-13 19:25:45 +0545

    feat: Add skip button custom widget

--- lib/features/authentication/screens/onBoarding/custom_widgets/onboarding_circular_button.dart
+++ lib/features/authentication/screens/onBoarding/custom_widgets/onboarding_circular_button.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_app/features/authentication/controllers/onBoarding/onboarding_controller.dart';
-import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
